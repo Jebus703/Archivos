2a8b53edf36f285fb57a065b954094725e017ca2
Jesus Antonio Suarez Duarte <88163703+Jebus703@users.noreply.github.com>
2024-08-14 16:17:55 -0500

    actualizando..

--- Holamundo.py
+++ Holamundo.py
@@ -1,1 +1,8 @@
 print("Hola Mundo")
+print("hola"+'mundo')
+print(555)
+print("hola \'mundo\'")
+print("estas son dos lineas"+"linea 1 \n linea 2")
+print("550+5 es igual a =",550+5)
+print("como poner un apostrofe\' para tenerlo")
+print("asi se hace una barra invertida\\")
