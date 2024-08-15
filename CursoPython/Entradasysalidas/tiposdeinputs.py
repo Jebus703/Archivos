79ac10b1abd82e22d865ffe02d7c8b8ae0b7110a
Jesus Antonio Suarez Duarte <88163703+Jebus703@users.noreply.github.com>
2024-08-15 12:03:34 -0500

    codigo

--- CursoPython/Entradasysalidas/tiposdeinputs.py
+++ CursoPython/Entradasysalidas/tiposdeinputs.py
@@ -3,3 +3,15 @@ Created on 15/08/2024
 
 @author: jsuarezd
 '''
+#input para ingresar informacion
+input("dime tu nombre : ")
+#orden de ejecucion primero el input y despues el print
+print(input("dime tu nombre : "))
+#orden de ejecucion primero el input y despues el print concatenado con string
+print("tu nombre es  : "+input("dime tu nombre : "))
+#inputs concatenado con los string del print
+print("tu nombre es  : "+input("dime tu nombre : ")+" "+input("dime tu apellido : "))
+#inputs mejorado para una mejor comprension lectura de codigo
+print("tu nombre es  : "+(input("dime tu nombre : "))+" "+(input("dime tu apellido : ")))
+
+
